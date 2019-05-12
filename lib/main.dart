c286eb8e5a9167769b7fc6bc1318ac74b3033f44
Carlos Pedroza <carlos.pedrozav@gmail.com>
2019-05-11 19:40:58 -0500

    course advence

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import './product_manager.dart';
+import "./product_control.dart";
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -9,25 +12,42 @@ class MyApp extends StatefulWidget {
     return _MyAppState();
   }
 }
-class _MyAppState extends State<MyApp> {
 
+class _MyAppState extends State<MyApp> {
+  List<String> products = [];
+
+  void _addProduct(String product) {
+    setState(() {
+      products.add(product);
+      //debugger(when: _products.length > 2);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColorDark: Colors.deepOrange,
-        primaryColorLight: Color.fromRGBO(255, 204, 188, 1),
-        primaryColor: Color.fromRGBO(255, 87, 34, 1),
-        accentColor: Colors.lightGreen,
-        dividerColor: Color.fromRGBO(189, 189, 189, 1)
-      ),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Easy List'),
-          ),
-          body: ProductManager(startingProduct: 'Tester sample'),
-    ));
+        //debugShowMaterialGrid: true,
+        theme: ThemeData(
+            brightness: Brightness.light,
+            primaryColorDark: Colors.deepOrange,
+            primaryColorLight: Color.fromRGBO(255, 204, 188, 1),
+            primaryColor: Color.fromRGBO(255, 87, 34, 1),
+            accentColor: Colors.lightGreen,
+            dividerColor: Color.fromRGBO(189, 189, 189, 1)),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Easy List'),
+            ),
+            body: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: ProductControl(_addProduct),
+                ),
+                Container(
+                  child: ProductManager(products),
+                )
+              ],
+            )));
   }
 }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -2,54 +2,31 @@ import "package:flutter/material.dart";
 import './product_card.dart';
 
 class ProductManager extends StatefulWidget {
-  final String startingProduct;
-
-  ProductManager({this.startingProduct = 'Default product'}) {
-    print("[contructor ProductManager]");
-  }
+  final List<String> products;
+  ProductManager(this.products);
 
   @override
   State<StatefulWidget> createState() {
-    print("[create state ProductManager]");
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = [];
-
   @override
   void initState() {
-     print("[init state ProductManager]");
     super.initState();
-    this._products.add(widget.startingProduct);
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: RaisedButton(
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              onPressed: () {
-                setState(() {
-                   print("[set state ProductManager]");
-                  _products.add('new peoduct');
-                });
-              },
-              child: Text('Add product'),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(10.1),
-            child: ProductCard(_products), //must sustitud by producs cards
-          ),
-        ],
-      ),
+    return ListView(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.all(10.1),
+          child: ProductCard(widget.products), //must sustitud by producs cards
+        ),
+      ],
     );
   }
 }
